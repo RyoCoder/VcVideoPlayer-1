4357be0912aa611be0fed944cef86cf7f08d55ca
RyoCoder <89810037+RyoCoder@users.noreply.github.com>
2021-09-13 00:46:02 +0700

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -1,7 +1,7 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
-START_TEXT = '👋 `Hey My Name Is` **VcVideoPlayer** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
-DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
+HELP_TEXT =f"**SETTINGS** \n\n`1) Thêm Bot này vào Nhóm của bạn và Đặt nó làm Quản trị viên` \n2) **Add** @{ASSISTANT_NAME} **đến nhóm của bạn** \n\n**COMMANDS** \n\n- `/stream` **: Trả lời video hoặc tệp mà bạn muốn phát trực tuyến trong Vc.** \n- `/stopstream`  **: Dừng luồng**"
+START_TEXT = '👋 `Chào, tên mình là` **VcVideoPlayer** \n\n`Cho phép thưởng thức chế độ xem điện ảnh của trình phát video nhóm với bạn bè của bạn ❤️😊`'
+ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 Bot này được tạo ra để phát trực tuyến video trong các cuộc trò chuyện video nhóm qua điện tín bằng một số phương pháp từ WebRTC.` \n\n`💡 Powered by Ryostar the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
+DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Đây là một số nhà phát triển đã giúp đỡ trong việc tạo ra` @{ASSISTANT_NAME} `Bot.`"
